ed17d4389f5aaebf263cf2f745349737bf511838
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-11-30 19:31:17 +0100

    Opens and closes .logfile

--- logfind.c
+++ logfind.c
@@ -1,9 +1,12 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc) {
 	assert(argc > 1);
 	FILE * configuration_file = fopen(".logfind", "r");
 	assert(configuration_file);
+	// read data from cinfguration file
+	fclose(configuration_file);
 	return 0;
 }
